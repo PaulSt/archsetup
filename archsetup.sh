f313e9d9f5e5004c7ea05339147458ffdd2c8b44
Paul <ping.paul@hotmail.com>
2018-09-28 14:51:14 +0200

    added newsboat

--- archsetup.sh
+++ archsetup.sh
@@ -15,7 +15,7 @@ user="$(cat username.txt)"
 
 # install basics
 pacman -Syu --noconfirm --needed
-pacman -S --noconfirm --needed sudo git wget dmenu freetype2 libx11 libxft libxinerama libxext libxft xorg-fonts-misc ncurses wicd-gtk ttf-liberation xorg-server xorg-xrandr xorg-xev xorg-xinit feh gvim dialog zathura firefox diff-so-fancy acpi xorg-xsetroot alsa-utils mutt ctags xorg-xbacklight cowsay
+pacman -S --noconfirm --needed sudo git wget dmenu freetype2 libx11 libxft libxinerama libxext libxft xorg-fonts-misc ncurses wicd-gtk ttf-liberation xorg-server xorg-xrandr xorg-xev xorg-xinit feh gvim dialog zathura firefox diff-so-fancy acpi xorg-xsetroot alsa-utils mutt ctags xorg-xbacklight cowsay newsboat
 
 # give user permissions
 useradd -m -G wheel -s /bin/bash $user
